3145ad967ceb1bab8063abd8f3bcc173a0a3ecc8
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:02:24 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 10:02:24
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            for(int j=0;j<jewels.length();j++){
+            if(stones.charAt(i)==jewels.charAt(j)){
+                count++;
+                break;
+            }
+            }
+        }
+        return count;
+    }
+}
