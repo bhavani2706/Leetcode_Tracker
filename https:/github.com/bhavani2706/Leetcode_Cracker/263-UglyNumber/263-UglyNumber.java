532b48009de13eab9f06a1c3fab1b57308d00ab0
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:24 +0530

    Create 263-UglyNumber

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:49:24
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0)return false;
+        while(n%2==0) n/=2;
+        while(n%3==0)n/=3;
+        while(n%5==0)n/=5;
+        return (n==1);
+    }
+
+}
