4d3dee5b65f9a2e3d39ca170914c747044d9ca68
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:42 +0530

    Create 191-NumberOf1Bits

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:49:42
+class Solution {
+    public int hammingWeight(int n) {
+String bin="";
+int count=0;
+      while(n>0){
+        int digit=n%2;
+        bin=digit+bin;
+         n= n/2;
+      }
+      for(int i=0;i<bin.length();i++){
+        if(bin.charAt(i)=='1'){
+              count++;
+        
+      }
+      }
+      return count; 
+    }
+}
