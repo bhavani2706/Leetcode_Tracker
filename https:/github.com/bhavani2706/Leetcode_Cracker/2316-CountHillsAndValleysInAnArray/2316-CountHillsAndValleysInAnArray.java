4d7f9e216035f796b1516bd50a32aaae8523aa71
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:46:44 +0530

    Create 2316-CountHillsAndValleysInAnArray

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/2316-CountHillsAndValleysInAnArray/2316-CountHillsAndValleysInAnArray.java
@@ -0,0 +1,27 @@
+// Last updated: 09/07/2026, 09:46:43
+class Solution {
+    public int countHillValley(int[] nums) {
+        int count = 0;
+        
+        for (int i = 1; i < nums.length - 1; i++) {
+            
+            if (nums[i] == nums[i - 1]) continue;
+
+            int left = i - 1;
+           
+            while (left >= 0 && nums[left] == nums[i]) left--;
+
+            int right = i + 1;
+            
+            while (right < nums.length && nums[right] == nums[i]) right++;
+
+            if (left >= 0 && right < nums.length) {
+                if ((nums[left] < nums[i] && nums[right] < nums[i]) ||
+                    (nums[left] > nums[i] && nums[right] > nums[i])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
