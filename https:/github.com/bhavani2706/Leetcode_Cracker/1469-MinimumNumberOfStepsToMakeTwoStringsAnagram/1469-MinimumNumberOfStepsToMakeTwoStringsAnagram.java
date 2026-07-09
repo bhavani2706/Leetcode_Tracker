321cfbae525fa03f01cd58851ed3f23b5a1c6939
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:47:33 +0530

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,28 @@
+// Last updated: 09/07/2026, 09:47:33
+ class Solution {
+    public int minSteps(String s, String t) {
+        if (s.length() != t.length()) {
+            return -1;
+        }
+
+        int[] sCount = new int[26];
+        int[] tCount = new int[26];
+
+        for (char c : s.toCharArray()) {
+            sCount[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            tCount[c - 'a']++;
+        }
+
+        int steps = 0;
+        for (int i = 0; i < 26; i++) {
+            if (sCount[i] > tCount[i]) {
+                steps += sCount[i] - tCount[i];
+            }
+        }
+
+        return steps;
+    }
+}
