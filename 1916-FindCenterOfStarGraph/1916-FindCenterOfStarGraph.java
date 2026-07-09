404045f2406671e621353930d84b4914c521a6fa
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:01:19 +0530

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.java
@@ -0,0 +1,7 @@
+// Last updated: 09/07/2026, 10:01:19
+class Solution {
+    public int findCenter(int[][] e) {
+       return e[0][0]==e[1][0] || e[0][0]==e[1][1]?e[0][0]:e[0][1];
+        
+    }
+}
