48fed197d94804fff3c7c1324bf6779ec75d4f29
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:48:50 +0530

    Create 507-PerfectNumber

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 09/07/2026, 09:48:50
+
+public class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+
+        }
+        return sum == num;
+    }
+}
