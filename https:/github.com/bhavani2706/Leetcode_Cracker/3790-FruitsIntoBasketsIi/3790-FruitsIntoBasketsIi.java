935c8718d0e749d2436d946f67770c0907987322
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:45:50 +0530

    Create 3790-FruitsIntoBasketsIi

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/3790-FruitsIntoBasketsIi/3790-FruitsIntoBasketsIi.java
@@ -0,0 +1,26 @@
+// Last updated: 09/07/2026, 09:45:49
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count = 0;
+
+        for (int i = 0; i < fruits.length; i++) {
+            int fruit = fruits[i];
+            int placed = -1;
+
+            for (int j = 0; j < baskets.length; j++) {
+                if (baskets[j] >= fruit) {
+                    placed = j;
+                    break;
+                }
+            }
+
+            if (placed == -1) {
+                count++;
+            } else {
+                baskets[placed] = -1;
+            }
+        }
+
+        return count; 
+    }
+}
