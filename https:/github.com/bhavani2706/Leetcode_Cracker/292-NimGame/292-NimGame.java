4bfc07281cd1e92612fcd6fd68624040b68e4b3d
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:12 +0530

    Create 292-NimGame

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:49:12
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+
+        }
+    
+     return true;
+    }
+}
