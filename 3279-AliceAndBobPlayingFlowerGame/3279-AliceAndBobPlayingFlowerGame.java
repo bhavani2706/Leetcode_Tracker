cdfae520a329b6037b04bd0e7a87c1f9bd694ec8
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:00:19 +0530

    Create 3279-AliceAndBobPlayingFlowerGame

---
+++ 3279-AliceAndBobPlayingFlowerGame/3279-AliceAndBobPlayingFlowerGame.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 10:00:19
+class Solution {
+    public long flowerGame(int n, int m) {
+        long oddsInN = (n + 1L) / 2;        // number of odd x
+        long evensInN = n / 2;             // number of even x
+        long oddsInM = (m + 1L) / 2;        // number of odd y
+        long evensInM = m / 2;              // number of even y
+
+        return oddsInN * evensInM + evensInN * oddsInM;
+    }
+}
