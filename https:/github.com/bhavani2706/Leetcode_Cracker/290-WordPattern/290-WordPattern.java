1d6b04d990f225a119d76d83c42c5316388ee902
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:49:14 +0530

    Create 290-WordPattern

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/290-WordPattern/290-WordPattern.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:49:13
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) return false;
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if (map1.containsKey(c) && !map1.get(c).equals(w)) return false;
+            if (map2.containsKey(w) && map2.get(w) != c) return false;
+            map1.put(c, w);
+            map2.put(w, c);
+        }
+        return true;
+    }
+}
