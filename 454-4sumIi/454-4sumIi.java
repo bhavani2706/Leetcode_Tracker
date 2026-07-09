9dcf63d31d6d8e95a9a58df011d359be719ffafb
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:02:50 +0530

    Create 454-4sumIi

---
+++ 454-4sumIi/454-4sumIi.java
@@ -0,0 +1,29 @@
+// Last updated: 09/07/2026, 10:02:50
+import java.util.*;
+
+class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                int sum = A[i] + B[j];
+                if (map.containsKey(sum)) {
+                    map.put(sum, map.get(sum) + 1);
+                } else {
+                    map.put(sum, 1);
+                }
+            }
+        }
+        for (int i = 0; i < C.length; i++) {
+            for (int j = 0; j < D.length; j++) {
+                int sum = C[i] + D[j];
+                if (map.containsKey(-sum)) {
+                    count += map.get(-sum);
+                }
+            }
+        }
+
+        return count;
+    }
+}
