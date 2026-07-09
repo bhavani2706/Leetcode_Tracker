4e798aba7f7b93f0005b00e58499731b3170cbeb
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:00:56 +0530

    Create 2121-FindIfPathExistsInGraph

---
+++ 2121-FindIfPathExistsInGraph/2121-FindIfPathExistsInGraph.java
@@ -0,0 +1,28 @@
+// Last updated: 09/07/2026, 10:00:55
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] vis=new boolean[n];
+        q.add(source);
+        vis[source]=true;
+        while(q.size()!=0){
+            int k=q.remove();
+            if(k==destination) return true;
+            for(int i:adj.get(k)) {
+                if(!vis[i]) {
+                    q.add(i);
+                    vis[i]=true;
+                }
+            }
+       }
+       return false;
+    }
+}
