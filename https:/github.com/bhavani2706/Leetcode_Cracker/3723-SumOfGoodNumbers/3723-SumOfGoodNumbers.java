3a6d1ad6dfcf8b9cef6ddd12504074af6ab670b6
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:45:55 +0530

    Create 3723-SumOfGoodNumbers

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/3723-SumOfGoodNumbers/3723-SumOfGoodNumbers.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:45:54
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++)
+            if ((i - k < 0 || nums[i] > nums[i - k]) &&
+                (i + k >= nums.length || nums[i] > nums[i + k]))
+                sum += nums[i];
+        return sum;
+    }
+}
