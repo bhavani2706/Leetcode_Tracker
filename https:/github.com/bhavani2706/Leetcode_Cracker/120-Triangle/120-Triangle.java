1b15f44aacb79481ab3ce6edcd40edf04022f680
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:50:14 +0530

    Create 120-Triangle

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/120-Triangle/120-Triangle.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 09:50:14
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        for(int i = triangle.size() - 2; i >= 0; i--) {
+
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+
+                int min = Math.min(
+                    triangle.get(i + 1).get(j),
+                    triangle.get(i + 1).get(j + 1)
+                );
+
+                triangle.get(i).set(
+                    j,
+                    triangle.get(i).get(j) + min
+                );
+            }
+        }
+
+        return triangle.get(0).get(0);
+    }
+}
