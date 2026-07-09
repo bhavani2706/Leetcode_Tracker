4fd1dcc85f202e62cc5d1a154eec378caaf7ad15
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:48:23 +0530

    Create 748-LargestNumberAtLeastTwiceOfOthers

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/748-LargestNumberAtLeastTwiceOfOthers/748-LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,17 @@
+// Last updated: 09/07/2026, 09:48:23
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int  maxIndex=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>nums[maxIndex]){
+                maxIndex=i;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i !=maxIndex && nums[maxIndex]<2*nums[i]){
+                return -1;
+            }
+        }
+        return maxIndex;
+    }
+}
