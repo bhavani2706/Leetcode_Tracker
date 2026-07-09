900bbf690e7aac14e11fb871707fda96fa9828bb
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:45:43 +0530

    Create 3914-CheckIfAnyElementHasPrimeFrequency

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/3914-CheckIfAnyElementHasPrimeFrequency/3914-CheckIfAnyElementHasPrimeFrequency.java
@@ -0,0 +1,26 @@
+// Last updated: 09/07/2026, 09:45:43
+import java.util.*;
+
+public class Solution {
+    public boolean checkPrimeFrequency(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int x : nums) {
+            cnt.merge(x, 1, Integer::sum);
+        }
+
+        for (int freq : cnt.values()) {
+            if (isPrime(freq)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+}
