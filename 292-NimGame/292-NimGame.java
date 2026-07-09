d32c98989f612b8a58dec32d76018ea29999d161
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 10:03:20 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 10:03:19
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+
+        }
+    
+     return true;
+    }
+}
