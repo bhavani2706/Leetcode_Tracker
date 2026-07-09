e1bc82f6a386687e6bf668d6a7e24655ff133fd9
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:46:52 +0530

    Create 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/2112-MinimumDifferenceBetweenHighestAndLowestOfKScores/2112-MinimumDifferenceBetweenHighestAndLowestOfKScores.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:46:52
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        int n=nums.length;
+        if(k==1)
+        return 0;
+        Arrays.sort(nums);
+        int diff=Integer.MAX_VALUE;
+        for(int i=0;i+k<=n;i++){
+                 diff=Math.min(diff,nums[i+k-1]-nums[i]);
+            
+        }
+        return diff;
+    }
+}
