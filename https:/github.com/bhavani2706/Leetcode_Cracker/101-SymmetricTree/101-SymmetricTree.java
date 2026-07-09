bd6518542553bfd2216b116191df60c7047b11c9
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:50:26 +0530

    Create 101-SymmetricTree

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/101-SymmetricTree/101-SymmetricTree.java
@@ -0,0 +1,30 @@
+// Last updated: 09/07/2026, 09:50:25
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return mirror(root.left,root.right);
+    }
+    private boolean mirror(TreeNode t1,TreeNode t2){
+        if(t1==null && t2==null)
+        return true;
+        if(t1==null || t2==null)
+        return false;
+        if(t1.val!=t2.val) return false;
+        
+        return mirror(t1.left,t2.right)&& mirror(t2.left,t1.right);
+}
+}
