a4f683f0faff1ad88faf53a7224c40a4285e9c30
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:48:00 +0530

    Create 1013-FibonacciNumber

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:48:00
+class Solution {
+    public int fib(int n) {
+    if (n<=1) {
+            return n;
+        }
+        int a=0;
+        int b=1;
+   for (int i=2;i<=n;i++) {
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
