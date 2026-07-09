419b5ef9a4e57c9b6248adca54619e68d89889f5
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:47:58 +0530

    Create 1018-LargestPerimeterTriangle

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1018-LargestPerimeterTriangle/1018-LargestPerimeterTriangle.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 09:47:58
+import java.util.Arrays;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+        return 0;
+    }
+}
