e99aa8c708ea18173fc81415ada7e00a3e852aa5
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:48:44 +0530

    Create 485-MaxConsecutiveOnes

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 09:48:44
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return maxCount;
+    }
+}
