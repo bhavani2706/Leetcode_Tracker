a783816e4d0a1ed5fab29e1935ced80374408d04
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:47:48 +0530

    Create 1329-MinimumCostToMoveChipsToTheSamePosition

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1329-MinimumCostToMoveChipsToTheSamePosition/1329-MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,17 @@
+// Last updated: 09/07/2026, 09:47:48
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        return Math.min(evenCount, oddCount);
+    }
+}
