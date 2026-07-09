5ccf01c49555fb6df5f17b93868d1b23347cd440
Muthu Bhavani D <muthubhavani067@gmail.com>
2026-07-09 09:47:43 +0530

    Create 1411-ConvertBinaryNumberInALinkedListToInteger

---
+++ https:/github.com/bhavani2706/Leetcode_Cracker/1411-ConvertBinaryNumberInALinkedListToInteger/1411-ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 09:47:42
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int index=0;
+        ListNode temp=head;
+        while(temp!=null){
+            index=(index*2)+temp.val;
+            temp=temp.next;
+        }
+        return index;
+    }
+}
